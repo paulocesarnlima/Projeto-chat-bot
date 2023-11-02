a6a35f39f5a97c6c05da349c615f6cd99508f891
Paulo Lima <39253812+paulocesarnlima@users.noreply.github.com>
2023-11-02 08:51:19 -0300

    up

--- app.py
+++ app.py
@@ -3,18 +3,12 @@ import openai
 import os
 
 
-# Recupere o valor da variável de ambiente
-ChaveOpenai = os.getenv("OpenaiKey")
-
-# Verifique se a variável de ambiente foi definida
-# if ChaveOpenai is not None:
-#     print(f"O valor da variável de ambiente OpenaiKey é: {ChaveOpenai}")
-# else:
-#     print("A variável de ambiente OpenaiKey não está definida.")
+# ChaveOpenai = os.getenv("OpenaiKey")
 
 app = Flask(__name__)
 
-openai.api_key = ChaveOpenai
+# openai.api_key = ChaveOpenai
+openai.api_key = '*'
 
 @app.route("/")
 
@@ -24,19 +18,22 @@ def index():
 @app.route("/api", methods=["POST"])
 
 def api():
-    menssage = request.json.get("message")
+    message = request.json.get("message")
     completion = openai.ChatCompletion.create(
-    model = "gpt-3.5-turbo",
-    messages=[
-        {"role": "system", "content": "Você é um assistente de programação."},
-        {"role": "user", "content": menssage},
-    ]
+        model = "gpt-3.5-turbo",
+        messages=[
+            {"role": "system", "content": "Você é um assistente de programação."},
+        {"role": "user", "content": message},
+        ]
     )
     
-    if completion.choices(0).mensage!=None:
+    if completion.choices(0).mensage is not None:
         return completion.choices(0).message
     else:
         return 'Falha para geração da resposta!'
     
-    if __name__=='__main__':
-        app.run()
+if __name__== '__main__':
+    app.run()
+
+    
+  
